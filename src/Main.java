165d3ef5b62fb175e4ddd967af36bd6ccdcfdbf1
AndreyT <zanarions@gmail.com>
2023-07-26 10:04:47 +0300

    Change

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        int start = 250;
-        int x = 1500;
-        int bonus = x / 100;
+        int check = 250;
+        int deposit = 1500;
+        int bonus = deposit / 100;
 
-        if (x > 1000) {
-            System.out.println(bonus + start + x);
+        if (deposit > 1000) {
+            System.out.println(bonus + check + deposit);
         }else{
-            System.out.println(start + x);}
+            System.out.println(check + deposit);}
     }
 }
